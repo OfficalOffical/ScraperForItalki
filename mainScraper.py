70020d271cff2741dc1a0903cc3c5239bd6ac2e0
Sefa <mail@sefakalkan.com>
2022-10-03 19:47:31 +0300

    0.3

--- mainScraper.py
+++ mainScraper.py
@@ -11,37 +11,63 @@ def mainWebScraper():
 
 class scraperCreator():
 
-    def __init__(self, url, name, path):
+
+    def __init__(self, url):
         self.url = url
         self.driver = webdriver.Chrome()
+
+
+    def makeConnection(self):
+        self.driver.get(self.url)
+
+    def closeConnection(self):
+        self.driver.close()
+
+    def scroolydowniytimeywimey(self):
+
+        SCROLL_PAUSE_TIME = 1
+
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            # Scroll down to bottom
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+            # Calculate new scroll height and compare with last scroll height
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+    def buttonClickShowMore(self):
+        self.driver.find_element(By.CSS_SELECTOR, ".text-normal").click()
+
 
 
 
 
 def scrapeWebFromArchive():
-    tempCounter = 0
-    webHolder = []
-    driver = webdriver.Chrome()
-    # Open the URL
-    driver.get("https://www.italki.com/en/community/exercises?language=turkish")
-    SCROLL_PAUSE_TIME = 1
-    # Get scroll height
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        # Scroll down to bottom
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        driver.find_element(By.CSS_SELECTOR, ".text-normal").click()
-        # Wait to load page
-        time.sleep(SCROLL_PAUSE_TIME)
-        # Calculate new scroll height and compare with last scroll height
-        new_height = driver.execute_script("return document.body.scrollHeight")
+
+    url = "https://www.italki.com/en/community/exercises?language=turkish"
+
+    mainScrape = scraperCreator(url)
+
+    mainScrape.makeConnection()
 
 
 
-        if new_height == last_height:
-            break
-        last_height = new_height
-    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    mainScrape.scroolydowniytimeywimey()
+
+    mainScrape.buttonClickShowMore()
+
+
+
+    mainScrape.closeConnection()
+
+
     """# Find all the <a> tags
     links = soup.find_all('a', class_='au av aw ax ay az ba bb bc bd be bf bg bh bi')
     for link in links:
@@ -62,6 +88,6 @@ def scrapeWebFromArchive():
 
 
     # Close the browser
-    driver.quit()
+
     #--print(len(links))
 
